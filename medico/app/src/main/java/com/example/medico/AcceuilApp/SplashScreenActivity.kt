699a791dc6f2a608f900d6d0fc3128f78883c330
hassayouneM <mouna.hassayoune@gmail.com>
2021-12-21 15:46:04 +0100

    splash screen

---
+++ medico/app/src/main/java/com/example/medico/AcceuilApp/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.medico.AcceuilApp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.medico.MainActivity
+import com.example.medico.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+    }
+}
